59bd432073c05d358915a1b1b9cbda8ffd095e41
Vinod Mylapilli <vinodmylapilli380@gmail.com>
2023-02-10 15:06:45 +0530

    Database create and migration

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app  = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Create Models
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
 
 @app.route('/')
 def index():
